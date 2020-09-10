bd54e85fd7e2db17d6bd0541b91fe804d1b26df7
liyuansong <jothan_lee@hzatmwlkjwwgc.onexmail.com>
2020-09-11 02:36:51 +0800

    no message

---
+++ easypoi/src/main/java/com/per/easypoi/utils/TalentImportVerifyHandler.java
@@ -0,0 +1,25 @@
+package com.example.easypoi.utils;
+
+import cn.afterturn.easypoi.excel.entity.result.ExcelVerifyHandlerResult;
+import cn.afterturn.easypoi.handler.inter.IExcelVerifyHandler;
+import com.example.easypoi.model.PersonExportVo;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.StringJoiner;
+
+@Component
+public class TalentImportVerifyHandler implements IExcelVerifyHandler<PersonExportVo> {
+
+
+
+    @Override
+    public ExcelVerifyHandlerResult verifyHandler(PersonExportVo inputEntity) {
+        StringJoiner joiner = new StringJoiner(",");
+        // 根据姓名与手机号判断数据是否重复
+
+        return new ExcelVerifyHandlerResult(true);
+    }
+
+
+}
