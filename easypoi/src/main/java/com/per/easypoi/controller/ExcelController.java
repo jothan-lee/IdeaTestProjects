6f8562e3ed390601eadd433411f2791905f14472
liyuansong <jothan_lee@hzatmwlkjwwgc.onexmail.com>
2020-09-11 01:43:10 +0800

    easypoi初始化

---
+++ easypoi/src/main/java/com/per/easypoi/controller/ExcelController.java
@@ -0,0 +1,119 @@
+package com.per.easypoi.controller;
+
+import com.example.easypoi.model.PersonExportVo;
+import com.example.easypoi.utils.ExcelUtils;
+import javafx.scene.image.Image;
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Null;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * excel 导出控制器
+ *
+ * @author novel
+ * @date 2018/12/27
+ */
+@RestController
+@RequestMapping("excel")
+public class ExcelController {
+    /**
+     * 导出
+     *
+     * @param response
+     */
+    @RequestMapping(value = "/export", method = RequestMethod.GET)
+    public void exportExcel(HttpServletResponse response) throws IOException {
+        List<PersonExportVo> personList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            PersonExportVo personVo = new PersonExportVo();
+            personVo.setName("张三" + i);
+            personVo.setUsername("张三" + i);
+            personVo.setPhoneNumber("18888888888");
+            personVo.setImageUrl("/static/user1-128x128.jpg");
+            personList.add(personVo);
+        }
+        ExcelUtils.exportExcel(personList, "员工信息表", "员工信息", PersonExportVo.class, "员工信息", response);
+    }
+
+    /**
+     * 导出excel
+     *
+     * @return 结果
+     */
+    @GetMapping("/exportToFile")
+    public Map<String, Object> export() {
+        List<PersonExportVo> personList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            PersonExportVo personVo = new PersonExportVo();
+            personVo.setName("张三" + i);
+            personVo.setUsername("张三" + i);
+            personVo.setPhoneNumber("18888888888");
+            personVo.setImageUrl("/static/user1-128x128.jpg");
+            personList.add(personVo);
+        }
+        String fileName = ExcelUtils.exportExcelToFile(personList, "员工信息", PersonExportVo.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("fileName", fileName);
+        return map;
+    }
+
+    /**
+     * 导入
+     *
+     * @param file
+     */
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
+    public Object importExcel(@RequestParam("file") MultipartFile file) throws IOException {
+
+
+        List<PersonExportVo> vos = ExcelUtils.importExcel(file, PersonExportVo.class);
+        for (PersonExportVo vo : vos) {
+            if (StringUtils.isNotBlank(vo.getImageUrl())&&checkImage(vo.getImageUrl())) {
+
+                //转换成base64
+                //上传到七牛云，返回URL地址
+                //setImageUrl(URL)
+                File file1 = new File(vo.getImageUrl());
+                if (file1.isFile() && file1.exists()) {
+                    System.out.println("file1 = " + file1);
+                    file1.delete();
+                }
+            }
+        }
+        return vos;
+    }
+
+    /**
+     *
+     * @param pathImg 本地图片路径
+     * @return
+     */
+    public boolean checkImage(String pathImg) {
+        BufferedImage image = null;
+        boolean valid =true;
+        try {
+            image= ImageIO.read(new File(pathImg));
+            if (image == null || image.getWidth() <= 0 || image.getHeight() <= 0) {
+                valid = false;
+
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            valid=false;
+        }
+        return valid;
+    }
+
+}
---
+++ easypoi/src/main/java/com/per/easypoi/model/PersonExportVo.java
@@ -0,0 +1,41 @@
+package com.per.easypoi.model;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.lang.annotation.Target;
+
+/**
+ * @author lz
+ * @date 2018/12/27
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PersonExportVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    /**
+     * 姓名
+     */
+    @Excel(name = "姓名", orderNum = "0", width = 15,replace = {"0_男","1_女"})
+    private String name;
+
+    /**
+     * 登录用户名
+     */
+    @Excel(name = "用户名", orderNum = "1", width = 15)
+    private String username;
+
+    @Excel(name = "手机号码", orderNum = "2", width = 15)
+    private String phoneNumber;
+
+    /**
+     * 人脸图片
+     */
+    @Excel(name = "人脸图片", orderNum = "3", width = 15, height = 30, type = 2,imageType = 1)
+    private String imageUrl;
+}
