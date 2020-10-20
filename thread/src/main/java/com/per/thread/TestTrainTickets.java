e16ea07734e174a2705ce81454903e9dfb555788
liyuansong <jothan_lee@hzatmwlkjwwgc.onexmail.com>
2020-10-20 21:30:12 +0800

    同步代码块，同步方法

--- thread/src/main/java/com/per/thread/TestTrainTickets.java
+++ thread/src/main/java/com/per/thread/TestTrainTickets.java
@@ -6,17 +6,30 @@ package com.per.thread;
  * @Date: 2020-10-20 16:04
  * @Version: 1.3.*
  */
-public class TestTrainTickets implements Runnable{
+public class TestTrainTickets implements Runnable {
 
     private int ticketNum = 100;
-    @Override
-    public  void run() {
-        while (ticketNum>0) {
-            System.out.println("剩余的票数"+ticketNum);
-            ticketNum--;
-        }
 
+    @Override
+    public void run() {
+    //同步代码块
+        synchronized (TestTrainTickets.class) {
+            while (ticketNum > 0) {
+                System.out.println("剩余的票数" + ticketNum);
+                ticketNum--;
+            }
+        }
     }
+
+   /* @Override
+    public synchronized void run() {
+        //同步方法
+            while (ticketNum > 0) {
+                System.out.println("剩余的票数" + ticketNum);
+                ticketNum--;
+            }
+
+    }*/
 
     public static void main(String[] args) {
         TestTrainTickets tickets = new TestTrainTickets();
