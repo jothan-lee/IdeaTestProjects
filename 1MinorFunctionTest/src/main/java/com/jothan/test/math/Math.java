50074d91f3789784402c8a108ee9310cee8402b2
lanan_lee <yuansong.l@aistarfish.com>
2021-05-27 23:50:38 +0800

    练习项目方向调整

---
+++ 1MinorFunctionTest/src/main/java/com/jothan/test/math/Math.java
@@ -0,0 +1,17 @@
+/**
+ * Aistarfish.com Inc.
+ * Copyright (c) 2017-2021 All Rights Reserved.
+ */
+package
+		com.jothan.test.math;
+
+/**
+ * @author LanAn_Lee
+ * Created by on 2021-05-27 23:19
+ */
+public class Math {
+	public static void main(String[] args) {
+		System.out.println(1==1);
+	}
+
+}
