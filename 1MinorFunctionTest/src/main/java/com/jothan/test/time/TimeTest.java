ed3c95344f760ccca82241a05e6531d07a728455
lanan_lee <yuansong.l@aistarfish.com>
2021-06-20 20:04:49 +0800

    no msg

--- 1MinorFunctionTest/src/main/java/com/jothan/test/time/TimeTest.java
+++ 1MinorFunctionTest/src/main/java/com/jothan/test/time/TimeTest.java
@@ -6,6 +6,7 @@ package
 		com.jothan.test.time;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.jothan.test.time.util.DateUtils;
 
@@ -15,12 +16,28 @@ import com.jothan.test.time.util.DateUtils;
  */
 public class TimeTest {
 
+	private final static String     SEARCH_TYPE_ALL   = "all";
 	public static void main(String[] args) {
 		long l = DateUtils.twoDateBetweenDays(new Date(), new Date());
 		System.out.println("l = " + l);
 
 		Date date = DateUtils.addDay(-214, new Date());
 		System.out.println("date = " +DateUtils.parseTime(date));
+
+		String endDateString = "2020-10"+ "-01 00:00:00";
+		System.out.println("endDateString = " + endDateString);
+		Date newDate = DateUtils.formatDate(endDateString);
+		System.out.println("newDate = " + newDate);
+		Date date1 = DateUtils.addMonth(1, newDate);
+		System.out.println("date1 = " + date1);
+		AtomicReference<Boolean> isZYMini = new AtomicReference<>(Boolean.FALSE);
+		isZYMini.set(true);
+		System.out.println("isZYMini = " + isZYMini.get());
+
+		String a = "11111111111";
+		System.out.println("SEARCH_TYPE_ALL"+a);
+
+
 
 	}
 }
